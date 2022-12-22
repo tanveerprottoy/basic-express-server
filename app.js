d381f1d326b86fbfb10cf4b3090989f8c4953fe5
tanveerprottoy <tanveer.prottoy@gmail.com>
2022-12-22 17:59:20 +0600

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+// Constants
+const PORT = 5555;
+const HOST = "127.0.0.1";
+
+// App
+const app = express();
+app.get("/", (req, res) => {
+    res.send({
+        message: "Hello from Express Server"
+    });
+});
+
+app.get("/api/v1/resourses", (req, res) => {
+    res.send({
+        data: [
+            {
+                id: 1,
+                name: "resource " + 1
+            },
+            {
+                id: 2,
+                name: "resource " + 2
+            }
+        ]
+    });
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
